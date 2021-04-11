43fe8f50a410a89b56458db33e4661a546b52ecc
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2021-04-11 12:07:32 +0300

    Initial import

---
+++ doc_duprate.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import sys
+import re
+import logging
+import xml.etree.ElementTree as ET
+
+from argparse import ArgumentParser
+
+
+DESCRIPTION = 'Get document duplication rate from onion output'
+
+
+def argparser():
+    ap = ArgumentParser(description=DESCRIPTION)
+    ap.add_argument('file', nargs='+')
+    return ap
+
+
+def get_id(docstart):
+    element = ET.fromstring(f'{docstart}</doc>')
+    return element.attrib['id']
+
+
+def get_duprate(fn, options):
+    current_id = None
+    current_marks = []
+    with open(fn) as f:
+        for ln, l in enumerate(f, start=1):
+            l = l.rstrip('\n')
+            try:
+                mark, token = l.split('\t')
+                mark = int(mark)
+            except Exception as e:
+                logging.error(f'failed to parse {fn} line {ln}: {e}: {l}')
+                continue
+            if token.startswith('<doc'):
+                current_marks = []
+                try:
+                    current_id = get_id(token)
+                except Exception as e:
+                    logging.error(f'failed to parse {fn} line {ln}: {e}: {l}')
+                    current_id = None
+                    continue
+            if current_id is not None:
+                current_marks.append(mark)
+            if token.startswith('</doc>'):
+                rate = sum(current_marks)/len(current_marks)
+                print(f'{rate}\t{current_id}')
+                current_id = None
+                current_marks = []
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    for fn in args.file:
+        get_duprate(fn, args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ tsv_to_vert.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import logging
+
+from argparse import ArgumentParser
+
+
+DESCRIPTION = 'Convert TSV with ID and text fields to vertical file'
+
+
+# Escapes for XML attributes
+XML_ATTR_ESCAPES = [
+    ('&', '&amp;'),
+    ('<', '&lt;'),
+    ('>', '&gt;'),
+    ('"', '&quot;'),
+    # ("'", '&apos;'),   # assume " quotes, so not needed
+]
+
+
+def argparser():
+    ap = ArgumentParser(description=DESCRIPTION)
+    ap.add_argument('file', nargs='+')
+    return ap
+
+
+def xml_attr_escape(string):
+    # Escape string for use as XML attribute
+    for char, escape in XML_ATTR_ESCAPES:
+        string = string.replace(char, escape)
+    return string
+
+
+def tsv_to_vert(fn, options):
+    with open(fn) as f:
+        for ln, l in enumerate(f, start=1):
+            l = l.rstrip('\n')
+            try:
+                id_, text = l.split('\t')
+                text = json.loads(text)    # assume JSON escaping
+            except Exception as e:
+                logging.error(f'failed to parse {fn} line {ln}: {e}: {l}')
+                continue
+            print(f'<doc id="{xml_attr_escape(id_)}">')
+            for para in text.split('\n\n'):
+                print(f'<p>')
+                for token in para.split():
+                    print(token)
+                print(f'</p>')
+            print(f'</doc>')
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    for fn in args.file:
+        tsv_to_vert(fn, args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
