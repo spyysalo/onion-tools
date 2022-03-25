54a18d206a1406fb94b21d8f1219e38bbe6277bb
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2022-03-25 13:53:33 +0200

    Initial import

---
+++ jsonl_diff.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+from argparse import ArgumentParser
+from difflib import unified_diff
+
+
+def argparser():
+    ap = ArgumentParser()
+    ap.add_argument('original')
+    ap.add_argument('filtered')
+    return ap
+
+
+def skip_until(orig, filt_item):
+    for orig_line in orig:
+        orig_item = json.loads(orig_line)
+        if filt_item is not None and orig_item['id'] == filt_item['id']:
+            return orig_item
+        else:
+            print(f'Only in {orig.name}: {orig_item["id"]}')
+            for line in orig_item['text'].splitlines():
+                print('<', line)
+            print('-'*78)
+    return None
+
+
+def process(orig, filt, args):
+    for filt_line in filt:
+        filt_item = json.loads(filt_line)
+        orig_item = skip_until(orig, filt_item)
+        if orig_item is None:
+            break
+        orig_text = orig_item['text'].splitlines(True)
+        filt_text = filt_item['text'].splitlines(True)
+        if orig_text == filt_text:
+            continue
+        print(f'diff {orig_item["id"]}')
+        for line in unified_diff(orig_text, filt_text):
+            if line.startswith('---') or line.startswith('+++'):
+                pass
+            elif line.startswith('-'):
+                print('<', line[1:].rstrip('\n'))
+            elif line.startswith('+'):
+                print('>', line[1:].rstrip('\n'))
+        print('-'*78)
+    skip_until(orig, None)
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    with open(args.original) as orig:
+        with open(args.filtered) as filt:
+            process(orig, filt, args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
